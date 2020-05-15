94fd448e6ffbe14835c0c50e1e81896d4babadc6
Ally Parker <ally.parker@red-gate.com>
2020-05-15 15:44:19 +0100

    Add base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       white: '#fff',
 
       gray: {
+        base: '#b6b6b6',
         100: '#f2f2f2',
         200: '#ebebeb',
         300: '#dcdcdc',
@@ -19,6 +20,7 @@ module.exports = {
         900: '#222222',
       },
       red: {
+        base: '#cc0000',
         100: '#fff5f5',
         200: '#fed7d7',
         300: '#feb2b2',
@@ -30,6 +32,7 @@ module.exports = {
         900: '#742a2a',
       },
       orange: {
+        base: '#fc9003',
         100: '#ffebcc',
         200: '#ffd699',
         300: '#fec267',
@@ -37,6 +40,7 @@ module.exports = {
         500: '#fc9003',
       },
       yellow: {
+        base: '#fed100',
         100: '#fff9d7',
         200: '#fff4ae',
         300: '#ffee86',
@@ -44,6 +48,7 @@ module.exports = {
         500: '#fed100',
       },
       green: {
+        base: '#1aac1e',
         100: '#d1eed2',
         200: '#a3dea5',
         300: '#76cd78',
@@ -53,6 +58,7 @@ module.exports = {
         700: '#0e5b10',
       },
       blue: {
+        base: '#3c85df',
         100: '#d8e7f9',
         200: '#b1cef2',
         300: '#8ab6ec',
