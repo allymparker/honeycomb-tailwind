7bad05062a61568cbf3bab96aba32ea129f314bd
Ally Parker <ally.parker@red-gate.com>
2020-05-15 15:28:48 +0100

    Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -172,22 +172,8 @@ module.exports = {
     },
     fontFamily: {
       sans: [
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
+        '"Roboto"', '"Helvetica Neue"', 'Arial', 'sans-serif'
       ],
-      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
-      mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
     },
     fontSize: {
       xs: '0.75rem',
